a07d0881dac17396e6c4209cc92cee3c5a7d8924
Jordan <jordancdiaz04@gmail.com>
2026-03-20 04:16:38 -0400

    jwt middleware, node-alpine

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,8 +3,9 @@ import { Request, Response } from "express";
 import { pool } from "../config/setupDB";
 import { hashPassword, verifyPassword } from "../utils/passwordUtils";
 import { DB_USER } from "../models/DatabaseTypes";
+import jwt from "jsonwebtoken";
 
-// const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
+const JWT_SECRET = process.env.JWT_SECRET || "jwtsecret";
 
 // Register a new user
 export const register = async (req: Request, res: Response) => {
@@ -30,8 +31,25 @@ export const register = async (req: Request, res: Response) => {
       [username, email, hashedPassword],
     );
 
-    const user = result.rows[0];
-    res.status(201).json({ message: "User registered", user });
+    const user: DB_USER = result.rows[0];
+
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      })
+      .status(201)
+      .json({ message: "User registered", user });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
@@ -58,14 +76,25 @@ export const login = async (req: Request, res: Response) => {
 
     const isMatch = await verifyPassword(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: "Invalid credentials" });
+      return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    // const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-    //   expiresIn: "1h",
-    // });
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
 
-    res.json({ message: "Login successful" });
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      })
+      .json({ message: "Login successful" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,28 @@ import dotenv from "dotenv";
 dotenv.config({ override: true });
 
 import express from "express";
+import cookieParser from "cookie-parser";
 import type { Request, Response } from "express";
 import videoRoutes from "./routes/videoRoutes";
 import authRoutes from "./routes/authRoutes";
 import { setupDB } from "./config/setupDB";
+import { auth, AuthRequest } from "./routes/authMiddleware";
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/api/example", (req: Request, res: Response) => {
   res.status(200).json({
     message: "GET request successful! ok",
   });
+});
+
+app.get("/api/protected", auth, (req: AuthRequest, res) => {
+  res.json({ message: "You are authenticated", user: req.user });
 });
 
 app.use("/api/videos", videoRoutes);
